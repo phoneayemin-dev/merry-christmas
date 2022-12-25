08267bf23599891bf039959fbd48b40837f2f107
phoneayemin <phoneayemin@better.hr>
2022-12-25 18:40:25 +0630

    staring project for merry christmas

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+var snowClass = document.querySelector('.container')
+console.log(snowClass)
+// const node = document.createElement("span")
+// let count = 0
+// while( count <= 10){
+//   snowClass.appendChild(node)
+//   count++
+//   console.log(classNode)
+// }
